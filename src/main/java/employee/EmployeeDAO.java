e36031b843976fe33a391c5d0a3d8662d2182375
aayushj19 <iaayush.19@gmail.com>
2026-02-03 10:38:16 +0530

    set-up env variable

--- src/main/java/employee/EmployeeDAO.java
+++ src/main/java/employee/EmployeeDAO.java
@@ -1,15 +1,18 @@
 package employee;
-
+import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 public class EmployeeDAO {
-
-    private static final String URL = "jdbc:mysql://localhost:3306/Employee";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
